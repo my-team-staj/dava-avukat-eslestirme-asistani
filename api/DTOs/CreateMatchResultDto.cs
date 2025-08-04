e490ec79c1556dbe42336c8da556e2f9f7bce271
Faruk Serdar DEGIRMI <farukserdar.degirmi@gunhukuk.com.tr>
2025-08-04 11:02:06 +0300

    MSSQL Docker Compose Dosyası Oluşturuldu bağlantılar yapıldı.

--- api/DTOs/CreateMatchResultDto.cs
+++
@@ -1,9 +0,0 @@
-using dava_avukat_eslestirme_asistani.DTOs;
-
-public class CreateMatchResultDto
-{
-    public int CaseId { get; set; }
-    public int LawyerId { get; set; }
-    public decimal Score { get; set; }
-    public bool IsPrimary { get; set; }
-}
--- api/DTOs/UpdateWorkingGrupDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dava_avukat_eslestirme_asistani.DTOs
-{
-    public class UpdateWorkingGroupDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        // Gerekirse başka alanlar da ekleyebilirsin
-    }
-}
