d0c4ee1215af803d4c6376876e7236ea69a6276a
Berat Çalık <berat.calik@gunhukuk.com.tr>
2025-08-04 16:24:09 +0300

    hata düzeltidi

--- api/Program.cs
+++ api/Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
